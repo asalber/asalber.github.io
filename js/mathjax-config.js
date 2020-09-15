c5d11736405aba20da4481f290ea1d9dceeb0fea
Alfredo Sánchez Alberca <asalber@gmail.com>
2020-09-16 00:10:49 +0200

    rebuilding site mié 16 sep 2020 00:10:49 CEST

---
+++ js/mathjax-config.js
@@ -0,0 +1,15 @@
+// MathJax Configuration
+//
+// v2 to v3 upgrade notes:
+// - The CommonHTML.linebreaks option is not yet implemented (but may be in a future release)
+// - The TeX.noUndefined.attributes option is not yet implemented (but may be in a future release)
+window.MathJax = {
+  loader: {
+    load: ['[tex]/noerrors']
+  },
+  tex: {
+    inlineMath: [['$', '$'], ['\\(', '\\)']],
+    displayMath: [['$$', '$$'], ['\\[', '\\]']],
+    packages: {'[+]': ['noerrors']}
+  }
+};
